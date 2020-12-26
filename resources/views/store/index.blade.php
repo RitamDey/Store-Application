b8e612489378391bcbd6ab1b758ff120e7aaa759
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 16:51:49 +0530

    WIP: Store homepage
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -1,4 +1,19 @@
-@foreach ($products as $product)
-    {{ $product->name }} <br/>
-    {{ $product->price }} <br/>
-@endforeach
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    @foreach ($products as $product)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    {{ $product->name }} {{ $product->price }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</x-app-layout>
