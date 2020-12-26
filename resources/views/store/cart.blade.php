712570afab164469d6db51afb5520529588fae3c
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 23:30:03 +0530

    Remove useless styles and boxes from the cart page
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/store/cart.blade.php
+++ resources/views/store/cart.blade.php
@@ -3,28 +3,14 @@
 @section("content")
     <style>
         body {
-            background: #eecda3;
-            background: -webkit-linear-gradient(to right, #eecda3, #ef629f);
-            background: linear-gradient(to right, #eecda3, #ef629f);
             min-height: 100vh;
         }
     </style>
     <div class="px-4 px-lg-0">
-        <!-- For demo purpose -->
-        <div class="container text-white py-5 text-center">
-            <h1 class="display-4">Bootstrap 4 shopping cart</h1>
-            <p class="lead mb-0">Build a fully structred shopping cart page using Bootstrap 4. </p>
-            <p class="lead">Snippet by <a href="https://bootstrapious.com/snippets" class="text-white font-italic">
-                    <u>Bootstrapious</u></a>
-            </p>
-        </div>
-        <!-- End -->
-
         <div class="pb-5">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
-
                         <!-- Shopping cart table -->
                         <div class="table-responsive">
                             <table class="table">
@@ -93,23 +79,6 @@
                 </div>
 
                 <div class="row py-5 p-4 bg-white rounded shadow-sm">
-                    <div class="col-lg-6">
-                        <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Coupon code</div>
-                        <div class="p-4">
-                            <p class="font-italic mb-4">If you have a coupon code, please enter it in the box below</p>
-                            <div class="input-group mb-4 border rounded-pill p-2">
-                                <input type="text" placeholder="Apply coupon" aria-describedby="button-addon3" class="form-control border-0">
-                                <div class="input-group-append border-0">
-                                    <button id="button-addon3" type="button" class="btn btn-dark px-4 rounded-pill"><i class="fa fa-gift mr-2"></i>Apply coupon</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Instructions for seller</div>
-                        <div class="p-4">
-                            <p class="font-italic mb-4">If you have some information for the seller you can leave them in the box below</p>
-                            <textarea name="" cols="30" rows="2" class="form-control"></textarea>
-                        </div>
-                    </div>
                     <div class="col-lg-6">
                         <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Order summary </div>
                         <div class="p-4">
