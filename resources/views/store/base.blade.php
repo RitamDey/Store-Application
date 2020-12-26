1f3b24e1ca888fd3744f77432f935f3e238096f4
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 18:18:15 +0530

    Completed the Navbar for every page in the application
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>
    
    This navbar is a dropdown navbar that displays the dashboard, cart and
    wishlist links if authenticated
    Otherwise displays the Login and Register links when not authenticated
    
    The Logout uses the form based POST method of logging out

--- resources/views/store/base.blade.php
+++ resources/views/store/base.blade.php
@@ -43,17 +43,25 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
-                    <h4 class="text-white">About</h4>
-                    <p class="text-muted">Add some information about the album below, the author, or any other
-                        background context. Make it a few sentences long so folks can pick up some informative tidbits.
-                        Then, link them off to some social networking sites or contact information.</p>
                 </div>
                 <div class="col-sm-4 offset-md-1 py-4">
-                    <h4 class="text-white">Contact</h4>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                        <li><a href="#" class="text-white">Like on Facebook</a></li>
-                        <li><a href="#" class="text-white">Email me</a></li>
+                        @guest
+                            <li><a href="{{ @route('login') }}" class="text-white">Login</a></li>
+                            <li><a href="{{ @route('register') }}" class="text-white">Register</a></li>
+                            <li><a href="#" class="text-white">Email me</a></li>
+                        @endguest
+                        @auth
+                            <li><a href="{{ @route('dashboard') }}" class="text-white">{{ \Illuminate\Support\Facades\Auth::user()->name }}</a></li>
+                            <li><a href="#" class="text-white">View Cart</a></li>
+                            <li><a href="#" class="text-white">View Wishlist</a></li>
+                            <li><form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a href="{{ @route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                                    </form></li>
+                        @endauth
                     </ul>
                 </div>
             </div>
@@ -68,7 +76,7 @@
                     <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
                     <circle cx="12" cy="13" r="4"/>
                 </svg>
-                <strong>Album</strong>
+                <strong> The Quick Store</strong>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
                     aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
