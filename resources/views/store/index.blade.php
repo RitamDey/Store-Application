98ecc6abfb7063f77084c1e2287597d85ce8ddf3
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 22:17:56 +0530

    Refactor and added dynamic logic to create links to product details page.
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -5,22 +5,22 @@
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 @foreach ($products as $product)
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="{{ $product->url }}" alt="{{ $product->name }} Image"/>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img src="{{ $product->url }}" alt="{{ $product->name }} Image"/>
 
-                        <div class="card-body">
-                            <p class="card-text"> {{ $product->name }} </p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <div class="card-body">
+                                <p class="card-text"> {{ $product->name }} </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="{{ @route("store.product", $product->id) }}" type="button"
+                                           class="btn btn-sm btn-outline-secondary">View</a>
+                                    </div>
+                                    <small class="text-muted">Rs. {{ $product->price }}</small>
                                 </div>
-{{--                                <small class="text-muted">9 mins</small>--}}
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
 
                 <div class="col">
