32027f084fc4c0c6a65a6a6a572d712592aaf1c5
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 22:03:23 +0530

    WIP form for adding a new product to the cart
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/store/details.blade.php
+++ resources/views/store/details.blade.php
@@ -16,8 +16,12 @@
                 <h3 class="my-3">Product Description</h3>
                 <p>{{ $product->description }}</p> <br/>
                 <h3> Rs. {{ $product->price }}</h3>
-            </div>
 
+                <form method="POST" id="cart-form">
+                    @csrf
+                    <input type="submit" value="Add to Cart">
+                </form>
+            </div>
         </div>
         <!-- /.row -->
     </div>
