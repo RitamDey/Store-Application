531d44a75141a2a334579dab0e8972daadbb8556
Ritam Dey <deyritam27031999@gmail.com>
2020-12-26 16:51:23 +0530

    Made the navbar handle guest and authenticated sessions
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,7 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @auth
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -32,7 +33,6 @@
                             </div>
                         </button>
                     </x-slot>
-
                     <x-slot name="content">
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
@@ -46,6 +46,15 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @endauth
+                @guest
+                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                        Register
+                    </x-nav-link>
+                    <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                        Login
+                    </x-nav-link>
+                @endguest
             </div>
 
             <!-- Hamburger -->
@@ -78,8 +87,8 @@
                 </div>
 
                 <div class="ml-3">
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+{{--                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>--}}
+{{--                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>--}}
                 </div>
             </div>
 
