d04f48cb34ca1247ccc2ead070ac9d3667a0687f
Ritam Dey <deyritam27031999@gmail.com>
2020-12-27 13:36:35 +0530

    Added timezones and added a new product in the seeded data
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -14,19 +14,25 @@ class ProductsSeeder extends Seeder {
                 'name' => "Product 1",
                 'description' => "Our first ever product",
                 'price' => 100,
-                'created_at' => Carbon::yesterday()
+                'created_at' => Carbon::yesterday("IST")
             ],
             [
                 'name' => "Product 2",
                 'description' => "Our second product",
                 'price' => 150,
-                'created_at' => Carbon::today()
+                'created_at' => Carbon::today("IST")
             ],
             [
                 'name' => "Product 3",
                 'description' => "Our latest innovation",
                 'price' => 300,
-                'created_at' => Carbon::tomorrow()
+                'created_at' => Carbon::tomorrow("IST")
+            ],
+            [
+                'name' => "Product 4",
+                'description' => "Because why not?",
+                'price' => 200,
+                'created_at' => Carbon::tomorrow("IST")->addDays(2)
             ]
         ]);
     }
