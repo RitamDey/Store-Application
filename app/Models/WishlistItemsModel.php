a3784f932a45cb68c55cab4d0db19bde0ff8f1f3
Ritam Dey <deyritam27031999@gmail.com>
2020-12-27 22:11:25 +0530

    WIP models for Wishlist models
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- app/Models/WishlistItemsModel.php
+++ app/Models/WishlistItemsModel.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WishlistItemsModel extends Model
-{
+class WishlistItemsModel extends Model {
     use HasFactory;
+
+    protected $attributes = [
+        "id",
+        "description",
+        "user_id",
+        "created_at",
+        "updated_at"
+    ];
+    protected $guarded = [
+        "id",
+        "created_at",
+        "updated_at"
+    ];
+    protected $fillable = [
+        "description",
+        "user_id"
+    ];
 }
--- app/Models/WishlistModel.php
+++ app/Models/WishlistModel.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WishlistModel extends Model
-{
+class WishlistModel extends Model {
     use HasFactory;
+
+    protected $primaryKey = "wishlist_id";
+    protected $fillable = [
+        "wishlist_id",
+        "product_id"
+    ];
+    protected $guarded = [
+        "added_at"
+    ];
+    protected $attributes = [
+        "wishlist_id",
+        "product_id",
+        "added_at"
+    ];
+
+    const CREATED_AT = "added_at";
 }
