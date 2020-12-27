f1463469dcbf51c57520f3518630dcf0d3252cf2
Ritam Dey <deyritam27031999@gmail.com>
2020-12-28 03:59:56 +0530

    Added the jQuery CDN
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -25,6 +25,8 @@
 </head>
 <body>
 {{--@include("components.navbar")--}}
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <header>
     <div class="collapse bg-dark" id="navbarHeader">
         <div class="container">
