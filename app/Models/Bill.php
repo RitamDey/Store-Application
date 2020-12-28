9adff8ad3d959650ffb7801a7b8f09f229044a2f
Ritam Dey <deyritam27031999@gmail.com>
2020-12-29 03:49:45 +0530

    WIP models for the bill tables
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

---
+++ app/Models/Bill.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill extends Model {
+    use HasFactory;
+
+    protected $attributes = [
+        "id",
+        "user",
+        "total_cost",
+        "total_items",
+        "created_at"
+    ];
+    protected $guarded = [
+        "id",
+        "created_at"
+    ];
+    protected $fillable = [
+        "user",
+        "total_cost",
+        "total_items"
+    ];
+    protected $primaryKey = "id";
+    protected $table = "bills";
+    public $timestamps = false;
+
+    /**
+     * Denotes the relationship b/w User model and the Bill model
+     * Using this the caller can resolve to which user the bill belongs to
+     * @return \App\Models\User
+    */
+    public function user() {
+        return $this->belongsTo(User::class, "user");
+    }
+
+    /**
+     * Denotes the relationship b/w Bill model and BillItems model
+     * Using this the caller can get the items present in a particular bill
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    **/
+    public function items() {
+        return $this->hasMany(BillItems::class, "bill_id");
+    }
+}
---
+++ app/Models/BillItems.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BillItems extends Model {
+    use HasFactory;
+
+    protected $table = "bill_items";
+    protected $fillable = [
+        "bill_id",
+        "product_id",
+        "quantity"
+    ];
+    public $timestamps = false;
+
+    /**
+     * Denotes the relation b/w BillItem model and Bill model
+     * Using this method, a caller can get the bill to which it belongs
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    **/
+    public function bill() {
+        return $this->belongsTo(Bill::class, "bill_id", "id");
+    }
+}
