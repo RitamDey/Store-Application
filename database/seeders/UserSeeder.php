187434c9f7a68bbc7416062ba84455391cb9e804
Ritam Dey <deyritam27031999@gmail.com>
2020-12-29 20:51:15 +0530

    Re-write the User seeders
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>
    
    Re-write the Users seeder to use the User model and fire the Registered event after insertion.
    This allows the creation of default wishlists for seeded users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+
 
 class UserSeeder extends Seeder {
     /**
@@ -12,25 +15,18 @@ class UserSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        DB::table("users")->insert([
-            [
-                "name" => "Ritam Dey",
-                "email" => "ritam@domain.com",
-                "email_verified_at" => null,
-                "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
-                "remember_token" => null,
-                "created_at" => Carbon::yesterday("IST"),
-                "updated_at" => Carbon::today("IST")
-            ],
-            [
-                "name" => "Tester",
-                "email" => "tester@domain.com",
-                "email_verified_at" => null,
-                "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
-                "remember_token" => null,
-                "created_at" => Carbon::yesterday("IST"),
-                "updated_at" => Carbon::today("IST")
-            ],
+        $ritam = User::create([
+            "name" => "Ritam Dey",
+            "email" => "ritam@domain.com",
+            "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
         ]);
+        event(new Registered($ritam));
+
+        $tester = User::create([
+            "name" => "Tester",
+            "email" => "tester@domain.com",
+            "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
+        ]);
+        event(new Registered($tester));
     }
 }
