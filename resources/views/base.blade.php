173457d4770a940ffd3a3ba8fbd4d060e83fbd1b
Ritam Dey <deyritam27031999@gmail.com>
2021-01-02 03:44:12 +0530

    Added bootbox.js as dependency
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -108,5 +108,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"
+        integrity="sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg=="
+        crossorigin="anonymous"></script>
 </body>
 </html>
