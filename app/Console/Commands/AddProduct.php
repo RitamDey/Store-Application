8260551e23cdec3c2d4ff1173dd64cdb3ce9d049
Ritam Dey <deyritam27031999@gmail.com>
2021-01-03 14:14:13 +0530

    Command logic for reading and adding products to database
    Signed-off-by: Ritam Dey <deyritam27031999@gmail.com>

--- app/Console/Commands/AddProduct.php
+++ app/Console/Commands/AddProduct.php
@@ -15,13 +15,37 @@ class AddProduct extends Command {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
+    public function handle() {
+        $file = $this->argument("file");
+        $exists = file_exists($file);
+        if ($exists === false) {
+            $this->error("$file doesn't exists");
+            return -1;
+        }
+        $contents = file_get_contents($file);
+        $this->info("Read the file");
+        $json = json_decode($contents);
+
+        foreach ($json as $key => $value) {
+            if ((property_exists($value, "name") === false)||(property_exists($value, "price") === false)) {
+                $this->error("Record misses important data");
+                continue;
+            }
+            if (property_exists($value, "url") === false)
+                $value->url = "http://www.flyermakerpro.com/_mobile/images/placeholder_logo.jpg";
+            if (property_exists($value, "description") === false)
+                $value->description = null;
+
+            $record = Products::create([
+                "name" => $value->name,
+                "description" => $value->description,
+                "url" => $value->url,
+                "price" => $value->price
+            ]);
+
+            $this->info($record->toJson());
+        }
+
         return 0;
     }
 }
